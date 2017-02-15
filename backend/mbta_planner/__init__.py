0a03ab497ec417086909ee611382f6400220f110
Kacper Adach <kacperadach@gmail.com>
2017-02-15 12:29:15 -0500

    changed init

--- backend/mbta_planner/__init__.py
+++ backend/mbta_planner/__init__.py
@@ -4,12 +4,11 @@ import flask
 from flask import jsonify
 
 from mbta_planner.views.user import user_view
-from mbta_planner.models.db_models import *
 
 app = flask.Flask(__name__)
 app.config['IMAGE_FILE_DIRECTORY'] = os.path.join(os.path.dirname(__file__), 'data')
 
-app.register_blueprint(user_view, url_prefix="/user")
+app.register_blueprint(user_view, url_prefix="/train")
 
 @app.route('/')
 def main():
