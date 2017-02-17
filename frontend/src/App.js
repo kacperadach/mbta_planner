ca5e2c5aa8625bc7bd1a142634f9c2df3c4244ab
Kacper Adach <kacperadach@gmail.com>
2017-02-17 12:05:19 -0500

    a

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,16 +7,7 @@ import SampleComponent from './components/sampleComponent';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>It's the Memory Improvement App!</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <SampleComponent/>
-      </div>
     );
   }
 }
